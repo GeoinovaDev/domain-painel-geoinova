ae0389c1e1ee3700509d9d34ce74c4e057e295c8
necojr <necoengenheiro@gmail.com>
2023-05-12 10:32:47 -0300

    alterado parametro para persistir os dados do bucket storage

--- repository/deteccao_repository.go
+++ repository/deteccao_repository.go
@@ -3,6 +3,6 @@ package repository
 import "github.com/GeoinovaDev/domain-painel-geoinova/core"
 
 type DeteccaoRepository interface {
-	UpdateBucket(int32, string, string, string) error
+	UpdateBucket(core.Deteccao) error
 	FetchByAnaliseId(int32) ([]core.Deteccao, error)
 }
