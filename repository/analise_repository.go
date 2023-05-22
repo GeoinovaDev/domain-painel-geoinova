588d73c00d361cb9dd0d56743c71f0ee611644da
necojr <necoengenheiro@gmail.com>
2023-05-22 16:11:29 -0300

    retorna a analise atualizada no Update da analise

--- repository/analise_repository.go
+++ repository/analise_repository.go
@@ -4,7 +4,7 @@ import "github.com/GeoinovaDev/domain-painel-geoinova/core"
 
 type AnaliseRepository interface {
 	Insert(core.Analise) (core.Analise, error)
-	Update(core.Analise) error
+	Update(core.Analise) (core.Analise, error)
 	Delete(core.Analise) error
 	Fetch(id int32) (core.Analise, error)
 	FetchWkt(analiseId int32) (string, error)
