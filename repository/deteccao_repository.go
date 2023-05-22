9420fa3a1a742327c38d058a438804bc142222c4
necojr <necoengenheiro@gmail.com>
2023-05-22 12:19:37 -0300

    atualizado bucket deteccao

--- repository/deteccao_repository.go
+++ repository/deteccao_repository.go
@@ -4,5 +4,8 @@ import "github.com/GeoinovaDev/domain-painel-geoinova/core"
 
 type DeteccaoRepository interface {
 	UpdateBucket(core.Deteccao) error
+	UpdateAntesBucket(core.Deteccao) error
+	UpdateDepoisBucket(core.Deteccao) error
+	UpdateResultadoBucket(core.Deteccao) error
 	FetchByAnaliseId(int32) ([]core.Deteccao, error)
 }
