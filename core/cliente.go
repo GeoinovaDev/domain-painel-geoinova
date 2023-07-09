711efe3775bbcf2dc038900a71e82b7faec9ee52
necojr <necoengenheiro@gmail.com>
2023-07-08 22:25:30 -0300

    adicionado status e descricao no cliente

--- core/cliente.go
+++ core/cliente.go
@@ -1,10 +1,12 @@
 package core
 
 type Cliente struct {
-	Id   int32  `json:"id,omitempty"`
-	Nome string `json:"nome,omitempty"`
+	Id        int32  `json:"id,omitempty"`
+	Nome      string `json:"nome,omitempty"`
+	Descricao string `json:"descricao"`
+	Status    string `json:"status"`
 }
 
 func NewCliente(id int32, nome string) *Cliente {
-	return &Cliente{id, nome}
+	return &Cliente{id, nome, "", ""}
 }
--- core/cliente_builder.go
+++ core/cliente_builder.go
@@ -8,6 +8,18 @@ func NewClienteBuilder(id int32, nome string) ClienteBuilder {
 	return ClienteBuilder{NewCliente(id, nome)}
 }
 
+func (b ClienteBuilder) WithStatus(status string) ClienteBuilder {
+	b.cliente.Status = status
+
+	return b
+}
+
+func (b ClienteBuilder) WithDescricao(descricao string) ClienteBuilder {
+	b.cliente.Descricao = descricao
+
+	return b
+}
+
 func (b ClienteBuilder) Build() *Cliente {
 	return b.cliente
 }
